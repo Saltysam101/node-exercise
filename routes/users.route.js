8c53f3611ec9e71fc0ec8f28e44a83dc3b48ad4f
Sam Zudell <s.zudell98@gmail.com>
2023-04-16 19:20:21 -0400

    created routes for /api/test and /api/users

---
+++ routes/users.route.js
@@ -0,0 +1,53 @@
+import express from "express";
+import db from "../mockdb";
+
+let router = express.Router();
+
+router.get("/:id?", async(req, res, next) => {
+    try {
+        // TODO
+        const { id } = req.params;
+        let data
+        if (id) {
+            data = await db.getOne(id)
+        } else {
+            data = await db.getAll();
+        }
+        res.json(data)
+    } catch (error) {
+        next(error);
+    }
+})
+router.post("/", async(req, res, next) => {
+    try {
+        // TODO
+        let newUser = req.body;
+        let data = await db.add(newUser)
+        res.json(data);
+    } catch (error) {
+        next(error);
+    }
+})
+router.put("/:id", async(req, res, next) => {
+    try {
+        // TODO
+        const { id } = req.params;
+        let updatedUser = req.body;
+        let data = await db.update(id, updatedUser);
+        res.json(data);
+    } catch (error) {
+        next(error);
+    }
+})
+router.delete("/:id", async(req, res, next) => {
+    try {
+        // TODO
+        const { id } = req.params
+        let data = await db.remove(id);
+        res.json(data);
+    } catch (error) {
+        next(error);
+    }
+})
+
+export default router;
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 import express from "express";
 import config from "./config";
+import morgan from "morgan";
 // TODO: import router from routes/
-
+import apiRouter from "./routes/index";
 const app = express();
 
 app.use(express.json());
 
-// TODO: use the imported router to handle all requests
+app.use(morgan("dev"));
 
+// TODO: use the imported router to handle all requests
+app.use("/api", apiRouter);
 app.use((err, req, res, next) => {
-  console.error(err);
-  res.json({ name: err.name, msg: err.message });
+    console.error(err);
+    res.json({ name: err.name, msg: err.message });
 });
 
 app.listen(config.port, () => {
-  console.log(`Server listening on port ${config.port}...`);
+    console.log(`Server listening on port ${config.port}...`);
 });
